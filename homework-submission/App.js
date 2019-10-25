8232fab659dbc377d8761aef3f366488bb898bc0
Rahime YILDIZ <50872722+rcmzyldz@users.noreply.github.com>
2019-10-25 14:45:50 +0200

    I get some errors

---
+++ homework-submission/App.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react';
+import './App.css';
+import Search from './Search';
+import Result from '.Result'
+
+function App() {
+  const [search, setSearch] = useState('')
+  return (
+    <div className="App">
+      <Search setSearch={setSearch}/>
+      <Result search={search}/>
+    </div>
+  );
+}
+
+export default App;
